0513037c0dff59bcc179a6040f43c529296b1ddc
Kim SeungHyeon <80245821+SeungHyeon2@users.noreply.github.com>
2023-05-09 14:43:25 +0900

    Add a Solved Problem

---
+++ src/main/java/org/example/level1/lessons12928.java
@@ -0,0 +1,24 @@
+package org.example.level1;
+
+import java.util.ArrayList;
+
+public class lessons12928 {
+
+    public static void main(String[] args) {
+        int n = 12;
+        solution(n);
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        // ArrayList<Integer> ai = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if(n%i==0){
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
