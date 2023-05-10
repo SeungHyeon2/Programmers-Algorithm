b3a60dc95f6ae8ae3e9d430fc40af27189097eae
Kim SeungHyeon <80245821+SeungHyeon2@users.noreply.github.com>
2023-05-10 16:59:50 +0900

    Add a Solved Problem

---
+++ src/main/java/org/example/level1/lessons12969.java
@@ -0,0 +1,22 @@
+package org.example.level1;
+
+import java.util.Scanner;
+
+public class lessons12969 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        StringBuilder row = new StringBuilder();
+        for (int i = 0; i < a; i++) {
+            row.append("*");
+        }
+        for (int i = 0; i < b; i++) {
+            System.out.println(row);
+        }
+    }
+
+
+}
