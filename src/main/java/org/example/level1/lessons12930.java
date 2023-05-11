1030891361bbd33df8afbcb8c39602d74ecbee75
Kim SeungHyeon <80245821+SeungHyeon2@users.noreply.github.com>
2023-05-11 15:38:03 +0900

    Add a Solved Problem

---
+++ src/main/java/org/example/level1/lessons12930.java
@@ -0,0 +1,37 @@
+package org.example.level1;
+
+import java.util.Objects;
+
+public class lessons12930 {
+
+    public static void main(String[] args) {
+        String s = "try hello world ";
+        solution(s);
+    }
+
+    public static String solution(String n) {
+        StringBuilder answer = new StringBuilder();
+
+        String[] sa = n.split("");
+
+        int count = 0;
+
+        for (String s : sa) {
+            if (s.equals(" ")) {
+                answer.append(" ");
+                count = 0;
+            } else {
+                if (count == 0) {
+                    answer.append(s.toUpperCase());
+                } else if (count % 2 == 0) {
+                    answer.append(s.toUpperCase());
+                } else {
+                    answer.append(s.toLowerCase());
+                }
+                count++;
+            }
+        }
+
+        return answer.toString();
+    }
+}
