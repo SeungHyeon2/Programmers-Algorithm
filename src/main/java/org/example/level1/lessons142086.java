9dbd430d261cb53ba765cdb30b808315023e3260
Kim SeungHyeon <80245821+SeungHyeon2@users.noreply.github.com>
2023-05-17 17:20:32 +0900

    Add a Solved Problem

---
+++ src/main/java/org/example/level1/lessons142086.java
@@ -0,0 +1,37 @@
+package org.example.level1;
+
+import java.util.ArrayList;
+
+public class lessons142086 {
+
+    public static void main(String[] args) {
+        String s = "banana";
+        // [-1, -1, -1, 2, 2, 2]
+        solution(s);
+    }
+
+
+    public static int[] solution(String s){
+        int[] answer = new int[s.length()];
+
+        for(int i = 0; i < answer.length; i++){
+            for(int j = i; j >= 0; j--){
+                if(s.charAt(i)==s.charAt(j) && answer[i] == 0){
+                    answer[i] = i-j;
+                }
+            }
+
+            if(answer[i] == 0){
+                answer[i] = -1;
+            }
+        }
+
+        for(int x : answer){
+            System.out.println(x);
+        }
+
+
+        return answer;
+    }
+
+}
