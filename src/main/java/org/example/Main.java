ad5fbf4e641aaa4f6ff013374e0b58d1a9871ec2
Kim SeungHyeon <80245821+SeungHyeon2@users.noreply.github.com>
2023-06-12 15:50:00 +0900

    Add a Solved Problem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Commit Test");
+        System.out.println("프로그래머스 코딩테스트 연습");
+        System.out.println("Practice for Coding Test(Programmers)");
     }
 
 
