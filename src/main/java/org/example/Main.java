48569af058ae3650d1ece3f480ea9fd231d8e302
Kim SeungHyeon <arcanum726@gmail.com>
2024-05-10 15:55:45 +0900

    compnay local setting complish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,8 @@
 package org.example;
-
+import java.util.regex.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("프로그래머스 코딩테스트 연습");
-        System.out.println("Practice for Coding Test(Programmers)");
+        System.out.println("프로그래머스 문제 풀이");
     }
 
 
